0712bccfacde4da2c6bac469ec0abeeb5553c18a
Giorge Lucas <giorge.lucas94@gmail.com>
2021-09-08 17:44:02 -0300

    Layers

---
+++ backend/src/main/java/com/projeto/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,7 @@
+package com.projeto.dsvendas.repositories;
+
+import com.projeto.dsvendas.entities.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+}
